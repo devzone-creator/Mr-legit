4f6f41cd48bdda8ed3a96770bb87720525da54c0
devzone-creator <munkailasule3@gmail.com>
2025-07-09 14:03:54 +0000

    lost

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ErrorBoundary from './components/ErrorBoundary'
 import HomePage from './pages/HomePage'
@@ -7,19 +6,19 @@ import AdminPage from './pages/AdminPage'
 import './App.css'
 
 function App() {
-  return (
-    <ErrorBoundary>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </ErrorBoundary>
-  )
+    return (
+        <ErrorBoundary>
+            <Router>
+                <div className="min-h-screen bg-gray-50">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/products" element={<ProductsPage />} />
+                        <Route path="/admin" element={<AdminPage />} />
+                    </Routes>
+                </div>
+            </Router>
+        </ErrorBoundary>
+    )
 }
 
 export default App
